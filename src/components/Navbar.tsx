75b3dde06626d9ac792d55a1b267631707ead574
Estefanía Q. Lusby <estefaniaquevedolusby@gmail.com>
2025-08-05 16:31:21 +0200

    first commit, started repository, added header

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        {/* Logo */}
+        <Link href="/" className={styles.logo}>
+          <Image
+            src="/logo.png" // path inside public folder
+            alt="La Grupa DK"
+            width={60}
+            height={60}
+            className={styles.logoImage}
+          />
+        </Link>
+
+        {/* Menu */}
+        <ul className={styles.menu}>
+          <li><Link href="/quienes-somos">Quiénes Somos</Link></li>
+          <li><Link href="/que-hacemos">¿Qué hacemos?</Link></li>
+          <li><Link href="/recursos">Recursos</Link></li>
+          <li><Link href="/faq">Preguntas Frecuentes</Link></li>
+          <li><Link href="/contacto">Contactanos</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
