921992a8f66909d36d15f7ea5b564ec8daecda47
Estefanía Q. Lusby <estefaniaquevedolusby@gmail.com>
2026-03-06 14:44:48 +0100

    Prevent search engines from indexing Sanity Studio

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -7,13 +7,20 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../../sanity.config";
 
-export const dynamic = 'force-static'
+export const metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
-export { metadata, viewport } from 'next-sanity/studio'
+export const dynamic = "force-static";
+
+export { viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
